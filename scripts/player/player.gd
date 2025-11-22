8ba5a7222e5808ae0c1a48d78d8e4735c9dd3747
limbicnation <gero@limbicnation.com>
2025-11-22 03:13:46 +0100

    Enhance player: Add Camera2D and sprite flipping

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -19,6 +19,7 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
