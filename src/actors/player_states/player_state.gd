af3e88488cd9412c4b171a51a5e328727e8b52bb
limbicnation <gero@limbicnation.com>
2025-11-24 06:20:56 +0100

    Fix: Resolve cyclic dependency between Player and PlayerState

--- src/actors/player_states/player_state.gd
+++ src/actors/player_states/player_state.gd
@@ -1,9 +1,9 @@
 class_name PlayerState extends State
 
-var player: Player
+var player
 
 func _ready() -> void:
 	if not owner.is_node_ready():
 		await owner.ready
-	player = owner as Player
+	player = owner as CharacterBody2D
 	assert(player != null, "PlayerState must be used within a Player scene.")
